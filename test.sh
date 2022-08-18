40c9267ff32dea960089b0e8ec6888b3796c4717
Emilplf <62566361+Emilplf@users.noreply.github.com>
2022-08-18 10:29:33 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 unclutter &
-chromium-browser https://erlectionede.dk https://erdettorsdag.dk --window-size=1920,1080 --start-fullscreen --kiosk --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic
+chromium-browser https://erlectionede.dk https://erdettorsdag.dk https://erdetfredag.dk --window-size=1920,1080 --start-fullscreen --kiosk --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic
